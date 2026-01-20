db5e826effcd7091a1a8d548e2f3152ab5ca21b7
彩花科技 <huadahuang1983@163.com>
2026-01-20 12:13:54 +0800

    fix: 修正初始数据

--- apps/web-antd/src/views/infra/registered-client/modules/form.vue
+++ apps/web-antd/src/views/infra/registered-client/modules/form.vue
@@ -50,7 +50,7 @@ const [Drawer, drawerApi] = useVbenDrawer({
         const scopes = values.scopes || 'openid,profile';
         const clientSettings =
           values.clientSettings ||
-          '{"settings.client.require-authorization-consent":true}';
+          '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-authorization-consent":true,"settings.client.require-proof-key":false}';
         const clientSettingsObj = JSON.parse(clientSettings);
         for (const [key, value] of Object.entries(clientSettingsObj)) {
           values[attributeNameToFieldName(key)] = Array.isArray(value)
@@ -59,7 +59,7 @@ const [Drawer, drawerApi] = useVbenDrawer({
         }
         const tokenSettings =
           values.tokenSettings ||
-          '{"settings.token.access-token-time-to-live":["java.time.Duration",7200.000000000],"settings.token.refresh-token-time-to-live":["java.time.Duration",3600.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",300.000000000],"settings.token.device-code-time-to-live":["java.time.Duration",300.000000000]}';
+          '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.access-token-time-to-live":["java.time.Duration",7200.000000000],"settings.token.refresh-token-time-to-live":["java.time.Duration",3600.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",300.000000000],"settings.token.device-code-time-to-live":["java.time.Duration",300.000000000]}';
         const tokenSettingsObj = JSON.parse(tokenSettings);
         for (const [key, value] of Object.entries(tokenSettingsObj)) {
           values[attributeNameToFieldName(key)] = Array.isArray(value)
